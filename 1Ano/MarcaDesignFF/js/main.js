cceea30f96c7fd6bb13c9b0dc28e0c9b1615db8d
Yzakiel1390 <yzakiel1390@gmail.com>
2024-09-17 02:40:19 -0300

    Quase todo o site pronto
    Todas as seções e rodapé praticamente finalizadas.

--- 1Ano/MarcaDesignFF/js/main.js
+++ 1Ano/MarcaDesignFF/js/main.js
@@ -8,4 +8,19 @@ function backgroundHeader() {
     }
 }
 
+function updateScrollMainSectors(multiplier) { // Move e atualiza o Scroll do "Main Sectors" section | multiplier é um valor que aceita 1 ou -1 para voltar ou seguir o scroll. (ñ sei explicar direito)
+    const mainSectors = document.querySelector(".main-sectors");
+    const mainSectorsWrapper = document.querySelector(".main-sectors-wrapper");
+    const wrapperLocationBarElements = document.querySelectorAll(".wrapper-location-bar div");
+
+    mainSectorsWrapper.scrollLeft += mainSectors.offsetWidth * multiplier;
+    mainSectorsControlButtons.forEach((element) => { element.toggleAttribute("disabled") });
+    wrapperLocationBarElements.forEach((element) => { element.classList.toggle("main-sectors-selected") });
+}
+
+const mainSectorsControlButtons = document.querySelectorAll(".main-sectors-control-buttons button"); // Acho q já exagerei um pouco mas blz
+
 document.addEventListener("scroll", backgroundHeader);
+mainSectorsControlButtons.forEach((element, index) => {
+    element.addEventListener("click", () => { updateScrollMainSectors(2 * index - 1) });
+});
